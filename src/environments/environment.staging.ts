5c4900397171dd2c796e7cde5f75a41b111bac00
Asif Shaikh <asif.shaikh@blueconchtech.com>
2021-07-30 10:49:02 +0530

    admin user change

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,7 +4,7 @@ export const environment = {
     production: true,
     environmentName: 'Staging',
     domain: 'dah.xpanxion.co.in',
-    admin_username: 'aimlworkbenchblueconchtechcom',
+    admin_username: 'asifshaikhblueconchtechcom',
     // admin_username: 'aimlworkbenchblueconchtechcom',
 
     appUrl: 'https://aimlworkbench.blueconchtech.com/',
